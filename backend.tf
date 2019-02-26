e668a73a5e271b835d7687ebbf5084404d34fca4
Bobby <karma0@gmail.com>
2019-02-26 11:38:18 -0700

    Changed example to terraform and add KMS

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 provider "aws" {}
 
 # create an S3 bucket to store the state file in
-resource "aws_s3_bucket" "example-state-storage-s3" {
-  bucket = "example-remote-state-storage-s3"
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "terraform-remote-state-storage-s3"
  
   versioning {
     enabled = true
@@ -18,8 +18,8 @@ resource "aws_s3_bucket" "example-state-storage-s3" {
 }
 
 # create a dynamodb table for locking the state file
-resource "aws_dynamodb_table" "dynamodb-example-state-lock" {
-  name = "example-state-lock-dynamo"
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
@@ -33,5 +33,10 @@ resource "aws_dynamodb_table" "dynamodb-example-state-lock" {
     Name = "DynamoDB Terraform State Lock Table"
   }
 
-  depends_on = ["aws_s3_bucket.example-state-storage-s3"]
+  depends_on = ["aws_s3_bucket.terraform-state-storage-s3"]
+}
+
+# create a KMS key for state encryption
+resource "aws_kms_key" "terraform-kms" {
+  description = "terraform KMS key"
 }
