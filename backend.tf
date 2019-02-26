542bbe4546fdd6c9c07cd1a1ef98fe5ffa2dd7c2
Bobby <karma0@gmail.com>
2019-02-26 11:43:53 -0700

    Added site name

--- backend.tf
+++ backend.tf
@@ -1,8 +1,13 @@
 provider "aws" {}
 
+variable "site" {
+  type = "string"
+  description = "A unique site name for allocation of resources"
+}
+
 # create an S3 bucket to store the state file in
 resource "aws_s3_bucket" "terraform-state-storage-s3" {
-  bucket = "terraform-remote-state-storage-s3"
+  bucket = "${var.site}-terraform-remote-state-storage-s3"
  
   versioning {
     enabled = true
@@ -19,7 +24,7 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
 
 # create a dynamodb table for locking the state file
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
+  name = "${var.site}-terraform-state-lock-dynamo"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
@@ -33,7 +38,7 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
     Name = "DynamoDB Terraform State Lock Table"
   }
 
-  depends_on = ["aws_s3_bucket.terraform-state-storage-s3"]
+  depends_on = ["aws_s3_bucket.${var.site}-terraform-state-storage-s3"]
 }
 
 # create a KMS key for state encryption
